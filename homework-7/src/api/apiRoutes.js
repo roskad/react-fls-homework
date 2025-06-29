d66e7ff8508e0a1879097afccd97f58517defeaa
roskad <ad.roskov@gmail.com>
2025-06-29 22:36:51 +0300

    Changed apiRoutes to secured connection

--- homework-7/src/api/apiRoutes.js
+++ homework-7/src/api/apiRoutes.js
@@ -1,7 +1,7 @@
 export default {
-  productsList: 'http://products-api-u6ni.onrender.com/api/products',
-  addProduct: 'http://products-api-u6ni.onrender.com/api/products',
-  getUpdateProductLink: (id) => `http://products-api-u6ni.onrender.com/api/products/${id}`,
-  getProductById: (id) => `http://products-api-u6ni.onrender.com/api/products/${id}`,
-  getDeleteProductLink: (id) => `http://products-api-u6ni.onrender.com/api/products/${id}`,
+  productsList: 'https://products-api-u6ni.onrender.com/api/products',
+  addProduct: 'https://products-api-u6ni.onrender.com/api/products',
+  getUpdateProductLink: (id) => `https://products-api-u6ni.onrender.com/api/products/${id}`,
+  getProductById: (id) => `https://products-api-u6ni.onrender.com/api/products/${id}`,
+  getDeleteProductLink: (id) => `https://products-api-u6ni.onrender.com/api/products/${id}`,
 }
