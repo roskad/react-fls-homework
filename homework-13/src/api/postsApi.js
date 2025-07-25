94f9dcb32d800d92d352b276307af2ba9c45b5f6
roskad <ad.roskov@gmail.com>
2025-07-25 16:06:38 +0300

    Update postsApi baseURL

--- homework-13/src/api/postsApi.js
+++ homework-13/src/api/postsApi.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const postsApi = createApi({
   reducerPath: 'postsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://posts-backend-48o1.onrender.com' }),
   tagTypes: ['Post', 'Posts'],
   endpoints: (build) => ({
     // Список постів з пагінацією (build.query)
